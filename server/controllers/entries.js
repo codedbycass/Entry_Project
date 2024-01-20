01f79ef493199dfe4e9520b14b10b039e6b2676c
cassmano <cassandramanotham@gmail.com>
2024-01-20 12:45:14 -0500

    added delete op udpated controller, routes, Feed.js

--- server/controllers/entries.js
+++ server/controllers/entries.js
@@ -48,3 +48,34 @@ export const getEntries = async (req, res) => {
         res.status(500).json({message: "Internal Server Error"})
     }
 }
+//UPDATE
+
+//DELETE
+export const deleteEntry = async (req, res) => {
+  try {
+    // Find post by ID
+    const entry = await Entry.findById(req.params.id)
+    // Check if post exists
+    if (!entry) {
+      return res.status(404).send({ message: "Post not found" })
+    }
+    // Check if authenticated user is creator of post
+    // if (entry.user.toString() !== req.user._id.toString()) {
+    //   return res
+    //     .status(403)
+    //     .send({ message: "Not authorized to delete this post" })
+    // }
+    // // Delete image from cloudinary if it exists
+    // if (post.cloudinaryId) {
+    //   await cloudinary2.uploader.destroy(post.cloudinaryId)
+    // }
+
+    // Delete post from MDB
+    await Entry.deleteOne({ _id: req.params.id })
+
+    res.status(200).send({ message: "Post deleted successfully" })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ message: "Error deleting post" })
+  }
+}
